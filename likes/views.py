a5c70786e346682139f34f9c5757d83d2eee1c94
Javi FS <javier.fersal@gmail.com>
2023-12-22 10:03:00 +0000

    Create LikeList and LikeDetail views

--- likes/views.py
+++ likes/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from drf_api.permissions import isOwnerReadOnly
+from .models import Like
+from .serializers import LikeSerializer
 
-# Create your views here.
+
+class LikeList(generics.ListCreateAPIView):
+    serializer_class = LikeSerializer
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly
+    ]
+    queryset = Like.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class LikeDetail(generics.RetrieveDestroyAPIView):
+    serializer_class = LikeSerializer
+    permission_classes = [
+        isOwnerReadOnly
+    ]
+    queryset = Like.objects.all()
